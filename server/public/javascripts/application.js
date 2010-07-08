ef381c70b2b363bc6f831d823fd5cddf2fdced38
David Lutterkort <lutter@apache.org>
2010-07-08 23:47:00 +0000

    Added JQuery wrapper for POST and DELETE actions
    git-svn-id: https://svn.apache.org/repos/asf/incubator/deltacloud/trunk@962334 13f79535-47bb-0310-9956-ffa450edef68

--- server/public/javascripts/application.js
+++ server/public/javascripts/application.js
@@ -1,2 +1,32 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+
+  $("a.delete").click(function(e) {
+    var original_url = $(this).attr('href')
+    $.ajax({
+      url : original_url,
+      type : 'DELETE',
+      cache : false,
+      success: function(data) {
+        window.location = original_url.replace(/\/([\w_-]+)$/i, '')
+      }
+    })
+    return false;
+  })
+
+  $("a.post").click(function(e) {
+    var original_url = $(this).attr('href')
+    $.ajax({
+      url : original_url,
+      type : 'POST',
+      dataType : 'xml',
+      success: function(data) {
+        window.location = original_url.replace(/\/([\w_-]+)$/i, '')
+      }
+    })
+    return false;
+  })
+
+})
