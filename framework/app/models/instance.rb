5ebaca983c63892503cfbbd7cbd31dac4a622663
David Lutterkort <lutter@apache.org>
2010-07-08 23:19:08 +0000

    Initialize actions to avoid NPE
    git-svn-id: https://svn.apache.org/repos/asf/incubator/deltacloud/trunk@962045 13f79535-47bb-0310-9956-ffa450edef68

--- framework/app/models/instance.rb
+++ framework/app/models/instance.rb
@@ -9,4 +9,8 @@ class Instance < BaseModel
   attr_accessor :public_addresses
   attr_accessor :private_addresses
   
+ def initialize(init=nil)
+   super(init)
+   self.actions = [] if self.actions.nil?
+  end  
 end
