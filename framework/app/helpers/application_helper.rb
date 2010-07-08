4abac265ed837599c528c6dea2c3b0f65ef93191
David Lutterkort <lutter@apache.org>
2010-07-08 23:19:32 +0000

    I love breadcrumbs.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/deltacloud/trunk@962051 13f79535-47bb-0310-9956-ffa450edef68

--- framework/app/helpers/application_helper.rb
+++ framework/app/helpers/application_helper.rb
@@ -1,4 +1,26 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
+  def bread_crumb
+    s = "<ul class='breadcrumb'><li class='first'><a href='/'>dCloud</a></li>"
+    url = request.path.split('?')  #remove extra query string parameters
+    levels = url[0].split('/') #break up url into different levels
+    levels.each_with_index do |level, index|
+      unless level.blank?
+        if index == levels.size-1 ||
+           (level == levels[levels.size-2] && levels[levels.size-1].to_i > 0)
+          s += "<li class='subsequent'>#{level.gsub(/_/, ' ')}</li>\n" unless level.to_i > 0
+        else
+            link = "/"
+            i = 1
+            while i <= index
+              link += "#{levels[i]}/"
+              i+=1
+            end
+            s += "<li class='subsequent'><a href=\"#{link}\">#{level.gsub(/_/, ' ')}</a></li>\n"
+        end
+      end
+    end
+    s+="</ul>"
+  end
 end
