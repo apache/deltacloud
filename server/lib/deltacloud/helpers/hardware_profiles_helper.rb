389d3673202ca30cbd7552e865a2619d00d94f3d
David Lutterkort <lutter@apache.org>
2010-07-08 23:43:26 +0000

    * hardware_profile_helper (format_hardware_property): add in unit
    git-svn-id: https://svn.apache.org/repos/asf/incubator/deltacloud/trunk@962286 13f79535-47bb-0310-9956-ffa450edef68

--- server/lib/deltacloud/helpers/hardware_profiles_helper.rb
+++ server/lib/deltacloud/helpers/hardware_profiles_helper.rb
@@ -2,14 +2,21 @@ module HardwareProfilesHelper
 
   def format_hardware_property(prop)
     return "&empty;" unless prop
+    u = hardware_property_unit(prop)
     case prop.kind
       when :range
-      "#{prop.first} - #{prop.last} (default: #{prop.default})"
+      "#{prop.first} #{u} - #{prop.last} #{u} (default: #{prop.default} #{u})"
       when :enum
-      prop.values.join(', ') + " (default: #{prop.default})"
+      prop.values.collect{ |v| "#{v} #{u}"}.join(', ') + " (default: #{prop.default} #{u})"
       else
-        prop.value.to_s
+      "#{prop.value} #{u}"
     end
   end
 
+  def hardware_property_unit(prop)
+    u = prop.unit
+    u = "" if ["label", "count"].include?(u)
+    u = "vcpus" if prop.name == :cpu
+    u
+  end
 end
