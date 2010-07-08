78334dbb0177979af7559f10d1d81d74ac38c73c
David Lutterkort <lutter@apache.org>
2010-07-08 23:19:22 +0000

    some line number cleanup
    git-svn-id: https://svn.apache.org/repos/asf/incubator/deltacloud/trunk@962049 13f79535-47bb-0310-9956-ffa450edef68

--- framework/app/models/instance.rb
+++ framework/app/models/instance.rb
@@ -8,11 +8,11 @@ class Instance < BaseModel
   attr_accessor :actions
   attr_accessor :public_addresses
   attr_accessor :private_addresses
-  
+
  def initialize(init=nil)
    super(init)
    self.actions = [] if self.actions.nil?
    self.public_addresses = [] if self.public_addresses.nil?
-   self.private_addresses = [] if self.private_addresses.nil?   
-  end  
+   self.private_addresses = [] if self.private_addresses.nil?
+  end
 end
