667e03bff8d91fd76e51866a1e64a175fc099bb4
David Lutterkort <lutter@apache.org>
2010-07-08 23:41:30 +0000

    Cucumber tests fixed for Ruby 1.8.6
    git-svn-id: https://svn.apache.org/repos/asf/incubator/deltacloud/trunk@962259 13f79535-47bb-0310-9956-ffa450edef68

--- server/features/support/env.rb
+++ server/features/support/env.rb
@@ -1,22 +1,25 @@
 require 'sinatra'
 require 'server'
 require 'rack/test'
-require 'base64'
+
+SERVER_DIR = File::expand_path(File::join(File::dirname(__FILE__), "../.."))
 
 Sinatra::Application.set :environment, :test
+Sinatra::Application.set :root, SERVER_DIR
 
 CONFIG = {}
-load 'features/support/configuration_mock.rb'
+load "features/support/configuration_mock.rb"
 
 World do
   def app
     @app = Rack::Builder.new do
+      ENV['API_DRIVER'] = "mock"
       run Sinatra::Application
     end
   end
 
   def replace_variables(str)
-    CONFIG[$DRIVER].each_key.collect { |k| str.gsub!(/\<#{k.to_s.upcase}\>/, "#{CONFIG[$DRIVER][k]}") }
+    CONFIG[$DRIVER].keys.collect { |k| str.gsub!(/\<#{k.to_s.upcase}\>/, "#{CONFIG[$DRIVER][k]}") }
     return str
   end
 
