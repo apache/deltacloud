c87cd5114389cf1ee9fd336cb5652a33e5700622
David Lutterkort <lutter@apache.org>
2010-10-19 22:46:37 +0000

    Disable blob streaming when not running under Thin
    Patch by Benjamin Browning <bbrowning@redhat.com>
    
    Fixes JIRA https://issues.apache.org/jira/browse/DTACLOUD-6
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/deltacloud/trunk@1024448 13f79535-47bb-0310-9956-ffa450edef68

--- server/lib/deltacloud/helpers/blob_stream.rb
+++ server/lib/deltacloud/helpers/blob_stream.rb
@@ -15,37 +15,46 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-#--
-# based on the example from http://macournoyer.com/blog/2009/06/04/pusher-and-async-with-thin/
-#--
-require 'eventmachine'
-class BlobStream
-  AsyncResponse = [-1, {}, []].freeze
-  def self.call(env, credentials, params)
-    body = DeferrableBody.new
-        #Get the headers out asap. Don't specify a content-type let
-        #the client guess and if they can't they SHOULD default to
-        #'application/octet-stream' anyway as per:
-        #http://www.w3.org/Protocols/rfc2616/rfc2616-sec7.html#sec7.2.1
-    EM.next_tick { env['async.callback'].call [200, {'Content-Type' => "#{params['content_type']}", 'Content-Length' => "#{params['content_length']}"}, body] }
-    #call the driver from here. the driver method yields for every chunk of blob it receives. We then
-    #use body.call to write that chunk as received.
-    driver.blob_data(credentials, params[:bucket], params[:blob], params) {|chunk| body.call ["#{chunk}"]} #close blob_data block
-    body.succeed
-    AsyncResponse # Tells Thin to not close the connection and continue it's work on other request
-  end
-end
-
-class DeferrableBody
-  include EventMachine::Deferrable
-
-  def call(body)
-    body.each do |chunk|
-      @body_callback.call(chunk)
+begin
+  require 'eventmachine'
+  #--
+  # based on the example from http://macournoyer.com/blog/2009/06/04/pusher-and-async-with-thin/
+  #--
+  class BlobStream
+    AsyncResponse = [-1, {}, []].freeze
+    def self.call(env, credentials, params)
+      body = DeferrableBody.new
+      #Get the headers out asap. Don't specify a content-type let
+      #the client guess and if they can't they SHOULD default to
+      #'application/octet-stream' anyway as per:
+      #http://www.w3.org/Protocols/rfc2616/rfc2616-sec7.html#sec7.2.1
+      EM.next_tick { env['async.callback'].call [200, {'Content-Type' => "#{params['content_type']}", 'Content-Length' => "#{params['content_length']}"}, body] }
+      #call the driver from here. the driver method yields for every chunk of blob it receives. We then
+      #use body.call to write that chunk as received.
+      driver.blob_data(credentials, params[:bucket], params[:blob], params) {|chunk| body.call ["#{chunk}"]} #close blob_data block
+      body.succeed
+      AsyncResponse # Tells Thin to not close the connection and continue it's work on other request
     end
   end
 
-  def each(&blk)
-    @body_callback = blk
+  class DeferrableBody
+    include EventMachine::Deferrable
+
+    def call(body)
+      body.each do |chunk|
+        @body_callback.call(chunk)
+      end
+    end
+
+    def each(&blk)
+      @body_callback = blk
+    end
+  end
+rescue LoadError => e
+  # EventMachine isn't available, disable blob streaming
+  class BlobStream
+    def self.call(env, credentials, params)
+      raise NotImplementedError.new("Blob streaming is only supported under Thin")
+    end
   end
 end
