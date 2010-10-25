a5320abd120500fbf1fea86eb68ea7e9ed072a37
David Lutterkort <lutter@apache.org>
2010-10-25 23:27:23 +0000

    site: move framework and client-ruby out of docs/
    git-svn-id: https://svn.apache.org/repos/asf/incubator/deltacloud/trunk@1027306 13f79535-47bb-0310-9956-ffa450edef68

--- site/deltacloud.org/lib/documentation.rb
+++ site/deltacloud.org/lib/documentation.rb
@@ -2,7 +2,7 @@ module DocumentationHelper
   def documentation_pages
     [
       { :href => 'documentation.html', :menu => 'Overview' },
-      { :href => "api.mdown" , :menu => 'REST API', :description => 'REST API definition' },
+      { :href => "api.html" , :menu => 'REST API', :description => 'REST API definition' },
       { :href => "drivers.html" , :menu => 'Drivers', :description => 'Currently-supported drivers' },
       { :href => "framework.html" , :menu => 'Framework', :description => 'Framework for writing additional drivers' },
       { :href => "client-ruby.html" , :menu => 'Ruby Client', :description => 'Ruby client' },
