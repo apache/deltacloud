ae203b99ea22764f4846470799087c2d438ff683
David Lutterkort <lutter@apache.org>
2010-10-26 00:02:17 +0000

    site: add news item about ApacheCon meetup
    git-svn-id: https://svn.apache.org/repos/asf/incubator/deltacloud/trunk@1027317 13f79535-47bb-0310-9956-ffa450edef68

--- site/lib/news.rb
+++ site/lib/news.rb
@@ -1,6 +1,7 @@
 module NewsHelper
   def all_news
     [
+      %Q(<a href="http://watzmann.net/blog/2010/10/deltacloud-at-apachecon.html">Deltacloud meetup</a> at ApacheCon on 11/4 at 8pm),
       %Q(Deltacloud Core <a href="http://watzmann.net/blog/2010/07/deltacloud-apache-incubator.html">moved to Apache Incubator</a>.),
       %Q(Deltacloud now <a href="./drivers.html#providers">supports GoGrid</a>!),
       %Q(We've introduced <a href="./api.html#h4_1">Hardware Profiles</a> to the API.),
