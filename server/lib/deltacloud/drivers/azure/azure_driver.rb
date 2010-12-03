f20a698fad39efe222a72b2cccd6010b1d79cd7b
Marios Andreou <marios@apache.org>
2010-12-03 17:26:43 +0000

    Adds create_blob and delete_blob for the Microsoft Azure driver
    git-svn-id: https://svn.apache.org/repos/asf/incubator/deltacloud/trunk@1041921 13f79535-47bb-0310-9956-ffa450edef68

--- server/lib/deltacloud/drivers/azure/azure_driver.rb
+++ server/lib/deltacloud/drivers/azure/azure_driver.rb
@@ -17,8 +17,9 @@
 # under the License.
 
 #Windows Azure (WAZ) gem at http://github.com/johnnyhalife/waz-storage
-require 'waz-blobs'
 require 'deltacloud/base_driver'
+require 'waz-blobs'
+
 module Deltacloud
   module Drivers
     module Azure
@@ -46,6 +47,9 @@ class AzureDriver < Deltacloud::BaseDriver
 # Create bucket
 #--
   def create_bucket(credentials, name, opts)
+    #for whatever reason, bucket names MUST be lowercase...
+    #http://msdn.microsoft.com/en-us/library/dd135715.aspx
+    name.downcase!
     bucket = nil
     azure_connect(credentials)
     safely do
@@ -85,6 +89,33 @@ class AzureDriver < Deltacloud::BaseDriver
     azure_connect(credentials)
     # WAZ get blob data methods cant accept blocks for 'streaming'... FIXME
       yield WAZ::Blobs::Container.find(bucket_id)[blob_id].value
+  end
+
+#--
+# Create Blob
+#--
+  def create_blob(credentials, bucket_id, blob_id, blob_data, opts=nil)
+    azure_connect(credentials)
+    #get a handle to the bucket in order to put there
+    the_bucket = WAZ::Blobs::Container.find(bucket_id)
+    the_bucket.store(blob_id, blob_data[:tempfile], blob_data[:type])
+    Blob.new( { :id => blob_id,
+                :bucket => bucket_id,
+                :content_lengh => blob_data[:tempfile].length,
+                :content_type => blob_data[:type],
+                :last_modified => ''
+            } )
+  end
+
+#--
+# Delete Blob
+#--
+  def delete_blob(credentials, bucket_id, blob_id, opts=nil)
+    azure_connect(credentials)
+    #get a handle to bucket and blob, and destroy!
+    the_bucket = WAZ::Blobs::Container.find(bucket_id)
+    the_blob = the_bucket[blob_id]
+    the_blob.destroy!
   end
 
   private
