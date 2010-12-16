77bff4357946d8b8d0c26e582944ab7bf13dab37
David Lutterkort <lutter@apache.org>
2010-12-16 23:43:26 +0000

    site: add roadmap to news; trim old items off news
    git-svn-id: https://svn.apache.org/repos/asf/incubator/deltacloud/trunk@1050233 13f79535-47bb-0310-9956-ffa450edef68

--- site/lib/news.rb
+++ site/lib/news.rb
@@ -1,11 +1,10 @@
 module NewsHelper
   def all_news
     [
-      %Q(<a href="http://watzmann.net/blog/2010/10/deltacloud-at-apachecon.html">Deltacloud meetup</a> at ApacheCon on 11/4 at 8pm),
+      %Q(<a href="roadmap.html">Roadmap</a> posted.),
+      %Q(<a href="http://watzmann.net/blog/2010/10/deltacloud-at-apachecon.html">Deltacloud meetup</a> at ApacheCon on 11/4 at 8pm.),
       %Q(Deltacloud Core <a href="http://watzmann.net/blog/2010/07/deltacloud-apache-incubator.html">moved to Apache Incubator</a>.),
-      %Q(Deltacloud now <a href="./drivers.html#providers">supports GoGrid</a>!),
-      %Q(We've introduced <a href="./api.html#h4_1">Hardware Profiles</a> to the API.),
-      %Q(Deltacloud <a href="http://press.redhat.com/2009/09/03/introducing-deltacloud/">announced</a> at 2009 Red Hat Summit!),
+      %Q(Deltacloud now <a href="./drivers.html#providers">supports GoGrid</a>!)
     ]
   end
 
