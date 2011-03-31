1b3f863aa57c339c9b6106c02ffe92a9b6e33aa1
Michal Fojtik <mfojtik@apache.org>
2011-03-31 08:34:48 +0000

    Added VCR gem to recort Test::Unit
    git-svn-id: https://svn.apache.org/repos/asf/incubator/deltacloud/trunk@1087213 13f79535-47bb-0310-9956-ffa450edef68

--- server/tests/drivers/rackspace/setup.rb
+++ server/tests/drivers/rackspace/setup.rb
@@ -1,3 +1,10 @@
 ENV['API_DRIVER']   = "rackspace"
-ENV['API_USER']     = '<RACKSPACE_USERNAME>'
-ENV['API_PASSWORD'] = '<RACKSPACE_API_KEY>'
+ENV['API_USER']     = 'michalfojtik'
+ENV['API_PASSWORD'] = '1f82168de18f91542834f1e861f37420'
+
+require 'vcr'
+VCR.config do |c|
+  c.cassette_library_dir = 'tests/rackspace/fixtures/'
+  c.stub_with :webmock
+  c.default_cassette_options = { :record => :new_episodes }
+end
