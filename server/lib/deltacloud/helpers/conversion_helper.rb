5fac044d33f1321c08f08931dd73a2f4400c43ee
David Lutterkort <lutter@apache.org>
2011-04-12 22:53:06 +0000

    Move Red Hat copyright to NOTICE
    git-svn-id: https://svn.apache.org/repos/asf/incubator/deltacloud/trunk@1091591 13f79535-47bb-0310-9956-ffa450edef68

--- server/lib/deltacloud/helpers/conversion_helper.rb
+++ server/lib/deltacloud/helpers/conversion_helper.rb
@@ -1,5 +1,3 @@
-# Copyright (C) 2009, 2010  Red Hat, Inc.
-#
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.  The
