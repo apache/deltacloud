d134b03b1089208c1be89901d1118e7c01f68f74
David Lutterkort <lutter@apache.org>
2011-04-28 22:33:16 +0000

    site: update driver matrices for 0.3.0
    git-svn-id: https://svn.apache.org/repos/asf/incubator/deltacloud/trunk@1097645 13f79535-47bb-0310-9956-ffa450edef68

--- site/lib/news.rb
+++ site/lib/news.rb
@@ -1,9 +1,9 @@
 module NewsHelper
   def all_news
     [
+      %Q(Deltacloud 0.3.0 is now available, including support for IBM SBC and Eucalyptus),
       %Q(The portal/aggregator is now part of the <a href="http://watzmann.net/blog/2011/01/aeolus-and-deltacloud.html">Aeolus</a> project.),
       %Q(<a href="roadmap.html">Roadmap</a> posted.),
-      %Q(<a href="http://watzmann.net/blog/2010/10/deltacloud-at-apachecon.html">Deltacloud meetup</a> at ApacheCon on 11/4 at 8pm.),
       %Q(Deltacloud Core <a href="http://watzmann.net/blog/2010/07/deltacloud-apache-incubator.html">moved to Apache Incubator</a>.),
       %Q(Deltacloud now <a href="./drivers.html#providers">supports GoGrid</a>!)
     ]
