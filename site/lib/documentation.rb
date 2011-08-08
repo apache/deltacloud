02060ce9ca3b3332b5166112aa3260341e0773a6
Christopher Lalancette <clalance@apache.org>
2011-08-08 17:15:43 +0000

    Add libdeltacloud API documentation to the website.
    Signed-off-by: Chris Lalancette <clalance@redhat.com>
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/deltacloud/trunk@1155020 13f79535-47bb-0310-9956-ffa450edef68

--- site/lib/documentation.rb
+++ site/lib/documentation.rb
@@ -6,6 +6,7 @@ module DocumentationHelper
       { :href => "drivers.html" , :menu => 'Drivers', :description => 'Currently-supported drivers' },
       { :href => "framework.html" , :menu => 'Framework', :description => 'Framework for writing additional drivers' },
       { :href => "client-ruby.html" , :menu => 'Ruby Client', :description => 'Ruby client' },
+      { :href => "libdeltacloud.html", :menu => "Libdeltacloud", :description => "C library" },
     ]
   end
 
