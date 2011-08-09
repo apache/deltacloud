adf8195716f90b4e67a874d2047eb7b44731a82b
Michal Fojtik <mfojtik@apache.org>
2011-08-09 20:16:03 +0000

    Mock: Support for user-data testing
    Signed-off-by: Michal fojtik <mfojtik@redhat.com>
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/deltacloud/trunk@1155541 13f79535-47bb-0310-9956-ffa450edef68

--- server/public/javascripts/application.js
+++ server/public/javascripts/application.js
@@ -17,6 +17,58 @@ $(document).ready(function() {
 
 })
 
+// NOTE: This code was copied from http://phpjs.org/functions/base64_encode:358
+// phpjs.org license it under the MIT and GPL licenses
+
+function encodeb64 () {
+  // Encodes string using MIME base64 algorithm
+  //
+  // version: 1107.2516
+  // discuss at: http://phpjs.org/functions/base64_encode    // +   original by: Tyler Akins (http://rumkin.com)
+  // +   improved by: Bayron Guevara
+  // +   improved by: Thunder.m
+  // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // +   bugfixed by: Pellentesque Malesuada    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // -    depends on: utf8_encode
+  // *     example 1: base64_encode('Kevin van Zonneveld');
+  // *     returns 1: 'S2V2aW4gdmFuIFpvbm5ldmVsZA=='
+  // mozilla has this native    // - but breaks in 2.0.0.12!
+  //if (typeof this.window['atob'] == 'function') {
+  //    return atob(data);
+  //}
+  var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";    var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+      ac = 0,
+      enc = "",
+      tmp_arr = [];
+
+  var data = $("textarea#user_data").attr('value');
+
+  do { // pack three octets into four hexets
+    o1 = data.charCodeAt(i++);
+    o2 = data.charCodeAt(i++);
+    o3 = data.charCodeAt(i++);
+    bits = o1 << 16 | o2 << 8 | o3;
+
+    h1 = bits >> 18 & 0x3f;
+    h2 = bits >> 12 & 0x3f;        h3 = bits >> 6 & 0x3f;
+    h4 = bits & 0x3f;
+
+    // use hexets to index into b64, and append result to encoded string
+    tmp_arr[ac++] = b64.charAt(h1) + b64.charAt(h2) + b64.charAt(h3) + b64.charAt(h4);    } while (i < data.length);
+
+    enc = tmp_arr.join('');
+
+    switch (data.length % 3) {    case 1:
+      enc = enc.slice(0, -2) + '==';
+      break;
+      case 2:
+      enc = enc.slice(0, -1) + '=';        break;
+    }
+
+    $("textarea#user_data").attr('value', enc);
+    return false;
+}
+
 function more_fields()
 {
   //increment the hidden input that captures how many meta_data are passed
