832ad5bef5370a7036c730973ed1acb6bb93b4bb
Michal Fojtik <mfojtik@apache.org>
2011-10-06 13:54:04 +0000

    RHEV-M: Disabled 'start' action when instance is in 'pending' state
    git-svn-id: https://svn.apache.org/repos/asf/incubator/deltacloud/trunk@1179625 13f79535-47bb-0310-9956-ffa450edef68

--- server/lib/deltacloud/drivers/rhevm/rhevm_driver.rb
+++ server/lib/deltacloud/drivers/rhevm/rhevm_driver.rb
@@ -64,7 +64,6 @@ class RHEVMDriver < Deltacloud::BaseDriver
 
   define_instance_states do
     start.to( :pending )          .automatically
-    pending.to( :running )        .on( :start )
     pending.to( :stopped )        .automatically
     stopped.to( :running )        .on( :start )
     running.to( :stopping )       .on( :stop )
