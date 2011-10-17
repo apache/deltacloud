e1ae64872694e663354fdd5354cc4b85d475e5e8
Michal Fojtik <mfojtik@apache.org>
2011-10-17 09:41:47 +0000

    Site: Replaced static news with twitter updates
    git-svn-id: https://svn.apache.org/repos/asf/incubator/deltacloud/trunk@1185060 13f79535-47bb-0310-9956-ffa450edef68

---
+++ site/content/js/tweet.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  if($.browser.msie) {
+    $("#dcloud_popup_link").attr("href", "dcloud.html");
+    $("#demo_popup_link").attr("href", "dcloud-demo.html");
+  } else {
+    $("a.vid").fancybox({ 'hideOnContentClick': true, 'width': 400, 'height': 300, 'titleShow': false });
+    $("a.providers").fancybox({ 'hideOnContentClick': true, 'padding': 0, 'margin': 0, 'width': 958,
+      'height': 640, 'scrolling': 'no', 'autoDimensions': false, 'autoScale': true });
+  }
+  $(".tweet").tweet({
+    username: "deltacloud",
+    query : '#deltacloud',
+    join_text: "auto",
+    avatar_size: 32,
+    count: 2,
+    retweets : false,
+    auto_join_text_default: "we said,",
+    auto_join_text_ed: "we",
+    auto_join_text_ing: "we were",
+    auto_join_text_reply: "we replied to",
+    auto_join_text_url: "we were checking out",
+    loading_text: "loading tweets..."
+  });
+});
