9b621023c0d237fbd68b33bef698a7d321751a5e
Michal Fojtik <mfojtik@apache.org>
2011-10-18 07:53:12 +0000

    Site: Fixed twitter query
    git-svn-id: https://svn.apache.org/repos/asf/incubator/deltacloud/trunk@1185518 13f79535-47bb-0310-9956-ffa450edef68

--- site/content/js/tweet.js
+++ site/content/js/tweet.js
@@ -9,16 +9,16 @@ $(document).ready(function() {
   }
   $(".tweet").tweet({
     username: "deltacloud",
-    query : '#deltacloud',
+    query : '#deltacloud OR @deltacloud',
     join_text: "auto",
     avatar_size: 32,
     count: 2,
     retweets : false,
     auto_join_text_default: "we said,",
-    auto_join_text_ed: "we",
-    auto_join_text_ing: "we were",
+    auto_join_text_ed: "",
+    auto_join_text_ing: "",
     auto_join_text_reply: "we replied to",
     auto_join_text_url: "we were checking out",
-    loading_text: "loading tweets..."
+    loading_text: "Loading news..."
   });
 });
