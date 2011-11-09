db39f9fb3740cf9748200cd82a9173350a436430
Marios Andreou <marios@apache.org>
2011-11-09 13:00:20 +0000

    Monkey patch fix for VCR normalisation code
    Needed in order to run google unit tests -
    see https://github.com/myronmarston/vcr/issues/101
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/deltacloud/trunk@1199729 13f79535-47bb-0310-9956-ffa450edef68

--- server/tests/drivers/google/setup.rb
+++ server/tests/drivers/google/setup.rb
@@ -11,3 +11,28 @@ VCR.config do |c|
   c.stub_with :excon
   c.default_cassette_options = { :record => :new_episodes}
 end
+
+#monkey patch fix for VCR normalisation code:
+#see https://github.com/myronmarston/vcr/issues/4
+#when body is a tempfile, like when creating new blob
+#this method of normalisation fails and excon throws errors
+#(Excon::Errors::SocketError:can't convert Tempfile into String)
+#
+#RELEVANT: https://github.com/myronmarston/vcr/issues/101
+#(will need revisiting when vcr 2 comes along)
+
+module VCR
+  module Normalizers
+    module Body
+
+    private
+    def normalize_body
+     self.body = case body
+          when nil, ''; nil
+          else
+            String.new(body) unless body.is_a?(Tempfile)
+        end
+      end
+    end
+  end
+end
