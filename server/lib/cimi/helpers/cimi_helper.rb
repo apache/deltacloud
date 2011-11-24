92b1c763a3789271f3cda91e118db1984b3c365d
Michal Fojtik <mfojtik@apache.org>
2011-11-24 13:19:27 +0000

    CIMI: Added a helper for rendering CIMI collections
    git-svn-id: https://svn.apache.org/repos/asf/deltacloud/trunk@1205815 13f79535-47bb-0310-9956-ffa450edef68

---
+++ server/lib/cimi/helpers/cimi_helper.rb
@@ -0,0 +1,42 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.  The
+# ASF licenses this file to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+module CIMIHelper
+end
+
+class Array
+  def to_xml_cimi_collection(_self)
+    model_name = first.class.xml_tag_name
+    XmlSimple.xml_out({
+      "xmlns" => "http://www.dmtf.org/cimi",
+      "uri" => [ _self.send(:"#{model_name.underscore.pluralize}_url") ],
+      "name" => [ "default" ],
+      "created" => [ Time.now.to_s ],
+      model_name => map { |model| { 'href' => model.uri } }
+    }, :root_name => "#{model_name}Collection")
+  end
+
+  def to_json_cimi_collection(_self)
+    model_name = first.class.xml_tag_name
+    {
+      "uri" => _self.send(:"#{model_name.underscore.pluralize}_url"),
+      "name" => "default",
+      "created" => Time.now.to_s,
+      model_name.pluralize.uncapitalize => map { |model| { 'href' => model.uri } }
+    }.to_json
+  end
+
+end
+
