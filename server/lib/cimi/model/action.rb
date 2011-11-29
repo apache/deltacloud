ea8ac859aa9919886a02f5f237d06f60abe0cd8b
Michal Fojtik <mfojtik@apache.org>
2011-11-29 15:31:38 +0000

    CIMI: Added Action model
    git-svn-id: https://svn.apache.org/repos/asf/deltacloud/trunk@1207914 13f79535-47bb-0310-9956-ffa450edef68

--- server/lib/cimi/model/action.rb
+++ server/lib/cimi/model/action.rb
@@ -12,23 +12,13 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
 # License for the specific language governing permissions and limitations
 # under the License.
-#
 
-# Declare namespace for CIMI model
-#
-module CIMI
-  module Model; end
+class CIMI::Model::Action < CIMI::Model::Base
+
+  text :action
+
+  def name
+    action.split('/').last.intern
+  end
+
 end
-
-require 'cimi/model/schema'
-require 'cimi/model/base'
-require 'cimi/model/machine_template'
-require 'cimi/model/machine_image'
-require 'cimi/model/machine_configuration'
-require 'cimi/model/action'
-require 'cimi/model/machine'
-require 'cimi/model/volume'
-require 'cimi/model/machine_admin'
-require 'cimi/model/volume_configuration'
-require 'cimi/model/volume_image'
-require 'cimi/model/volume_template'
