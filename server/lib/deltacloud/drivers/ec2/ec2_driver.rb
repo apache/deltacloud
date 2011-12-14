f85a9625c998972f000d8f0085c94c9b4be40034
Marios Andreou <marios@apache.org>
2011-12-14 17:17:08 +0000

    Add fetching of EC2 regions via API call (for drivers collection)
    git-svn-id: https://svn.apache.org/repos/asf/deltacloud/trunk@1214350 13f79535-47bb-0310-9956-ffa450edef68

--- server/lib/deltacloud/drivers/ec2/ec2_driver.rb
+++ server/lib/deltacloud/drivers/ec2/ec2_driver.rb
@@ -695,6 +695,12 @@ module Deltacloud
           end
         end
 
+        def providers(credentials, opts={})
+          ec2 = new_client(credentials)
+          providers = ec2.describe_regions.map{|r| Provider.new( {:id=>r, :name=>r,
+           :url=>"#{ENV['API_HOST']}:#{ENV['API_PORT']}#{settings.root_url}\;provider=#{r}" }) }
+        end
+
         def valid_credentials?(credentials)
           retval = true
           begin
