b276666ef389a05dd1a3728713954a1e26ec432c
Michal Fojtik <mfojtik@apache.org>
2012-01-23 13:53:38 +0000

    Mock: Removed unnecessary code in Cucumber environment file
    Signed-off-by: Michal fojtik <mfojtik@redhat.com>
    
    git-svn-id: https://svn.apache.org/repos/asf/deltacloud/trunk@1234795 13f79535-47bb-0310-9956-ffa450edef68

--- tests/mock/support/env.rb
+++ tests/mock/support/env.rb
@@ -1,48 +1,26 @@
-SERVER_DIR = File::expand_path(File::join(File::dirname(__FILE__), "../../../server"))
-
-$top_srcdir = SERVER_DIR
-$:.unshift File::join($top_srcdir, 'lib')
-
-ENV['API_DRIVER'] = 'mock'
-
-Dir.chdir(SERVER_DIR)
 require 'rubygems'
 require 'nokogiri'
-require 'deltacloud/server'
-require 'rack/test'
 
-Sinatra::Application.set :environment, :test
-Sinatra::Application.set :root, SERVER_DIR
+SERVER_DIR = File::expand_path(File::join(File::dirname(__FILE__), "../../../server"))
+$top_srcdir = SERVER_DIR
+$:.unshift File::join($top_srcdir, 'lib')
+Dir.chdir(SERVER_DIR)
+
+ENV['API_DRIVER'] = 'mock'
+ENV.delete('API_VERBOSE')
+load File.join($top_srcdir, 'lib', 'deltacloud', 'server.rb')
+
+require 'rack/test'
 
 CONFIG = {
   :username => 'mockuser',
   :password => 'mockpassword'
 }
 
-ENV['RACK_ENV']     = 'test'
+def output_xml
+  Nokogiri::XML(last_response.body)
+end
 
-World do
-
-  include Rack::Test::Methods
-
-  def app
-    @app = Rack::Builder.new do
-      set :environment => :test
-      set :loggining => true
-      set :raise_errors => true
-      set :show_exceptions => true
-      run Sinatra::Application
-    end
-  end
-
-  def output_xml
-    Nokogiri::XML(last_response.body)
-  end
-
-  Before do
-    unless @no_header
-      header 'Accept', 'application/xml'
-    end
-  end
-
+def app
+  Sinatra::Application
 end
