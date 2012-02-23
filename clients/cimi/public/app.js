5cf4f2ba5490714e3c2b5ced9b0cfbc185fd9f2c
Michal Fojtik <mfojtik@redhat.com>
2012-02-23 14:55:05 +0100

    CIMI: Updated client layout and views to look nice with bootstrap 2.0

--- clients/cimi/public/app.js
+++ clients/cimi/public/app.js
@@ -1,3 +1,17 @@
 $(function() {
   $(".alert-message").alert();
+
+  $("a#saveProvider").bind('click', function() {
+    $('#providerModal').modal('hide');
+    $("#providerModal form").submit();
+    return false;
+  })
+
 });
+
+function toggleConfig(driver) {
+  $('#providerModal form input[name=driver]').attr('value', driver);
+  $('#providerModal span#driverName').html(driver.toUpperCase());
+  $('#providerModal').modal('show');
+  return false;
+}
