dd79215753ce1114b73322e5d82a93304645c4c9
Michal Fojtik <mfojtik@redhat.com>
2012-04-17 15:17:00 +0200

    Core: Added Deltacloud::API class and helpers to serve the default entrypoint

--- server/lib/deltacloud/helpers.rb
+++ server/lib/deltacloud/helpers.rb
@@ -1,4 +1,3 @@
-#
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.  The
@@ -91,5 +90,6 @@ module Deltacloud::Collections
     def self.route_for(url)
       "#{settings.root_url}#{url}"
     end
+
   end
 end
