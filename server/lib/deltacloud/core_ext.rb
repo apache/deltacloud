5aa335beff48436fa913d0c145bfe9169f3a8342
Michal Fojtik <mfojtik@redhat.com>
2012-05-28 10:51:29 +0200

    Core: Fixed displaying drivers using deltacloudd --drivers
    Signed-off-by: Michal fojtik <mfojtik@redhat.com>

--- server/lib/deltacloud/core_ext.rb
+++ server/lib/deltacloud/core_ext.rb
@@ -14,6 +14,14 @@
 # under the License.
 #
 
+unless Kernel.respond_to?(:require_relative)
+  module Kernel
+    def require_relative(path)
+      require File.join(File.dirname(caller[0]), path.to_str)
+    end
+  end
+end
+
 require_relative './core_ext/array'
 require_relative './core_ext/hash'
 require_relative './core_ext/integer'
