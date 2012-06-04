8e5669ad88ccc77f18bde0aaf7a6f6beb859f1f9
Michal Fojtik <mfojtik@redhat.com>
2012-06-04 18:39:23 +0200

    Vsphere: Wrap connection timeout to vpshere with proper HTTP status code (DTACLOUD-225)

--- server/lib/deltacloud/drivers/vsphere/vsphere_driver.rb
+++ server/lib/deltacloud/drivers/vsphere/vsphere_driver.rb
@@ -373,6 +373,10 @@ module Deltacloud::Drivers::Vsphere
         status 502
       end
 
+      on /Connection timed out/ do
+        status 504
+      end
+
       on /execution expired/ do
         status 504
       end
