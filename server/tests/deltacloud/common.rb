ae2758fc4f7cd9fa7ae5e0a37b278406a9884296
Michal Fojtik <mfojtik@redhat.com>
2012-07-20 13:32:28 +0200

    Core: Fixed memory leaks on ruby 1.8.7
    Minitest library seems to load the 'common.rb' file in collection
    tests repeatly. This cause to require all DC libraries for every test,
    which until a while leads to memory corruption. Note this problem
    can be only observed under Ruby 1.8.7.
    
    This patch also bumps sinatra-rabbit to 1.0.10 (minor typo fixed there
    to make 1.8.7 tests run smoothly)

--- server/tests/deltacloud/common.rb
+++ server/tests/deltacloud/common.rb
@@ -21,11 +21,13 @@ ENV['RACK_ENV']   = 'test'
 
 # Setup Deltacloud::API Sinatra instance
 #
-Deltacloud::configure do |server|
-  server.root_url '/api'
-  server.version '1.0.0'
-  server.klass 'Deltacloud::API'
-  server.logger Rack::DeltacloudLogger.setup(ENV['API_LOG'], ENV['API_VERBOSE'])
-end
+unless Deltacloud::config[:deltacloud]
+  Deltacloud::configure do |server|
+    server.root_url '/api'
+    server.version '1.0.0'
+    server.klass 'Deltacloud::API'
+    server.logger Rack::DeltacloudLogger.setup(ENV['API_LOG'], ENV['API_VERBOSE'])
+  end
 
-Deltacloud.require_frontend!
+  Deltacloud.require_frontend!
+end
