73b5c596c5fbe606c548d2c0a889b24652985845
Michal Fojtik <mfojtik@redhat.com>
2012-08-17 12:01:08 +0200

    Core: Removed deprecated tests from server/tests directory
    * All tests should be now replaced using minitest

--- server/tests/deprecated/api/library_test.rb
+++
@@ -1,6 +0,0 @@
-require_relative './common'
-require_relative '../../lib/deltacloud/api'
-
-require 'pp'
-
-
--- server/tests/deprecated/drivers/mock/common.rb
+++
@@ -1,7 +0,0 @@
-ENV['API_DRIVER']   = 'mock'
-ENV['TESTS_API_USERNAME'] = 'mockuser'
-ENV['TESTS_API_PASSWORD'] = 'mockpassword'
-
-
-$:.unshift File.join(File.dirname(__FILE__), '..', '..', '..')
-require 'tests/minitest_common'
