2054a472ee658522fc02843df7508a84c3784acc
marios <marios@redhat.com>
2012-09-14 18:35:36 +0300

    CIMI: Adds NetworkPortTemplate (used to be VSPTemplate)

---
+++ server/lib/cimi/collections/network_port_templates.rb
@@ -0,0 +1,51 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.  The
+# ASF licenses this file to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+module CIMI::Collections
+  class NetworkPortTemplates < Base
+
+    set :capability, lambda { |m| driver.respond_to? m }
+
+    collection :network_port_templates do
+
+      description 'The NetworkPort Template is a set of Configuration values for realizing a NetworkPort. A NetworkPort Template may be used to create multiple NetworkPorts'
+
+      operation :index, :with_capability => :network_port_templates do
+        description 'List all NetworkPortTemplates in the NetworkPortTemplateCollection'
+        param :CIMISelect, :string, :optional
+        control do
+          network_port_templates = NetworkPortTemplateCollection.default(self).filter_by(params[:CIMISelect])
+          respond_to do |format|
+            format.xml {network_port_templates.to_xml}
+            format.json {network_port_templates.to_json}
+          end
+        end
+      end
+
+      operation :show, :with_capability => :network_port_templates do
+        description 'Show a specific NetworkPortTemplate'
+        control do
+          network_port_template = NetworkPortTemplate.find(params[:id], self)
+          respond_to do |format|
+            format.xml {network_port_template.to_xml}
+            format.json {network_port_template.to_json}
+          end
+        end
+      end
+
+    end
+
+  end
+end
---
+++ server/lib/cimi/models/network_port_template_collection.rb
@@ -0,0 +1,37 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.  The
+# ASF licenses this file to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+class CIMI::Model::NetworkPortTemplateCollection < CIMI::Model::Base
+
+  CIMI::Model.register_as_root_entity! "NetworkPortTemplates"
+
+  text :count
+
+  #add array of network_port_templates:
+  self << CIMI::Model::NetworkPortTemplate
+
+  def self.default(context)
+    network_port_templates = CIMI::Model::NetworkPortTemplate.all(context)
+    self.new(
+      :id => context.network_port_templates_url,
+      :name => 'default',
+      :created => Time.now,
+      :description => "#{context.driver.name.capitalize} NetworkPortTemplateCollection",
+      :count => network_port_templates.size,
+      :network_port_templates => network_port_templates
+    )
+  end
+
+end
