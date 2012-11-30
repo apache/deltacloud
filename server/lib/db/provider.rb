d9e21e027c47dfd062cf97d1a2746d820a95d404
Michal Fojtik <mfojtik@redhat.com>
2012-11-30 17:04:08 +0100

    CIMI: Fix strange issue with Fedora MRI and Datamapper associations

--- server/lib/db/provider.rb
+++ server/lib/db/provider.rb
@@ -10,6 +10,17 @@ module Deltacloud
 
       has n, :entities
       has n, :machine_templates
+
+      # This is a workaround for strange bug in Fedora MRI:
+      #
+      def machine_templates
+        MachineTemplate.all(:provider_id => self.id)
+      end
+
+      def entities
+        Entity.all(:provider_id => self.id)
+      end
+
     end
 
   end
