apache/deltacloud

commit 775cbdf26226b0460a577b61bb9ff44453bb4dbb

CIMI: make CIMI::Model::VolumeImage.find behave like Machine.find
TrackedAt: http://tracker.deltacloud.org/patch/31b0b82f80cff66df01e6a4cb3a6e88e91a24f6d

server/lib/cimi/models/volume.rb
@@ -38,12 +38,15 @@ class CIMI::Model::Volume < CIMI::Model::Base
   end
 
   def self.find(id, context)
-    volumes = []
-    opts = ( id == :all ) ? {} : { :id => id }
-    volumes = context.driver.storage_volumes(context.credentials, opts)
-    volumes.collect!{ |volume| from_storage_volume(volume, context) }
-    return volumes.first unless volumes.length > 1 || volumes.length == 0
-    return volumes
+    creds = context.credentials
+    if id == :all
+      volumes = context.driver.storage_volumes(creds)
+      volumes.collect{ |volume| from_storage_volume(volume, context) }
+    else
+      volume = context.driver.storage_volumes(creds)
+      raise CIMI::Model::NotFound unless volume
+      from_storage_volume(volume)
+    end
   end
 
   def self.all(context); find(:all, context); end

commit 70a00e422df6ce49f777c62274683778ae4deab6

CIMI: make retrieving individual volumes and volume images work again
Fixes problems introduced by commits 775cbdf2 and 14f9103e

server/lib/cimi/models/volume.rb
@@ -43,9 +43,9 @@ class CIMI::Model::Volume < CIMI::Model::Base
       volumes = context.driver.storage_volumes(creds)
       volumes.collect{ |volume| from_storage_volume(volume, context) }
     else
-      volume = context.driver.storage_volumes(creds)
+      volume = context.driver.storage_volumes(creds, :id => id).first
       raise CIMI::Model::NotFound unless volume
-      from_storage_volume(volume)
+      from_storage_volume(volume, context)
     end
   end
 

server/lib/cimi/models/volume_image.rb
@@ -30,9 +30,9 @@ class CIMI::Model::VolumeImage < CIMI::Model::Base
       snapshots = context.driver.storage_snapshots(creds)
       snapshots.collect{ |snapshot| from_storage_snapshot(snapshot, context) }
     else
-      snapshot = context.driver.storage_snapshots(creds)
+      snapshot = context.driver.storage_snapshots(creds, id => :id).first
       raise CIMI::Model::NotFound unless snapshot
-      from_storage_snapshot(snapshot)
+      from_storage_snapshot(snapshot, context)
     end
   end
 