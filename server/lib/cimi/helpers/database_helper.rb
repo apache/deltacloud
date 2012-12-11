apache/deltacloud

commit 1a1705079b335b38caadc1556629a1a51e48232d

CIMI: Moved helper methods out of db.rb to separe helper file

server/lib/cimi/helpers/database_helper.rb
@@ -1,0 +1,46 @@
+module Deltacloud
+  module Helpers
+
+    module Database
+
+      def test_environment?
+        Deltacloud.test_environment?
+      end
+
+      def store_attributes_for(model, values={})
+        return if test_environment?
+        return if model.nil? or values.empty?
+        current_db.entities.first_or_create(:be_kind => model.to_entity, :be_id => model.id).update(values)
+      end
+
+      def load_attributes_for(model)
+        return {} if test_environment?
+        entity = get_entity(model)
+        entity.nil? ? {} : entity.to_hash
+      end
+
+      def delete_attributes_for(model)
+        return if test_environment?
+        entity = get_entity(model)
+        !entity.nil? && entity.destroy!
+      end
+
+      def get_entity(model)
+        current_db.entities.first(:be_kind => model.to_entity, :be_id => model.id)
+      end
+
+      def current_provider
+        Thread.current[:provider] || ENV['API_PROVIDER'] || 'default'
+      end
+
+      # This method allows to store things into database based on current driver
+      # and provider.
+      #
+      def current_db
+        Provider.first_or_create(:driver => driver_symbol.to_s, :url => current_provider)
+      end
+
+    end
+  end
+
+end

server/lib/cimi/helpers.rb
@@ -26,11 +26,12 @@ end
 # Declare namespace for CIMI models
 #
 
-require_relative '../db'
 require_relative '../deltacloud/drivers'
 require_relative '../deltacloud/models'
 require_relative '../deltacloud/helpers/driver_helper'
 require_relative '../deltacloud/helpers/auth_helper'
 require_relative '../deltacloud/helpers/deltacloud_helper'
 require_relative '../deltacloud/helpers/rabbit_helper'
+
 require_relative './helpers/cimi_helper'
+require_relative './helpers/database_helper'

server/lib/cimi/server.rb
@@ -21,6 +21,8 @@ require 'sinatra/base'
 require 'sinatra/rabbit'
 require_relative '../sinatra'
 
+require_relative '../db'
+
 require_relative './helpers'
 require_relative './collections'
 

server/lib/db.rb
@@ -1,13 +1,7 @@
 module Deltacloud
 
   def self.test_environment?
-    ENV['RACK_ENV'] == 'test'
-  end
-
-  module Database
-    def test_environment?
-      Deltacloud.test_environment?
-    end
+    ENV['RACK_ENV'] == 'test' || ENV['DELTACLOUD_NO_DATABASE']
   end
 
   unless test_environment?
@@ -17,57 +11,15 @@ module Deltacloud
     require_relative './db/machine_template'
   end
 
-  DATABASE_LOCATION = ENV['DATABASE_LOCATION'] || "/var/tmp/deltacloud-mock-#{ENV['USER']}/db.sqlite"
+  DATABASE_LOCATION = ENV['DATABASE_LOCATION'] || File.join('/', 'var', 'tmp', "deltacloud-mock-#{ENV['USER']}", 'db.sqlite')
 
   def self.initialize_database
-    DataMapper::Logger.new($stdout, :debug)
+    DataMapper::Logger.new($stdout, :debug) if ENV['API_VERBOSE']
     dbdir = File::dirname(DATABASE_LOCATION)
-    unless File::directory?(dbdir)
-      FileUtils::mkdir(dbdir)
-    end
+    FileUtils::mkdir(dbdir) unless File::directory?(dbdir)
     DataMapper::setup(:default, "sqlite://#{DATABASE_LOCATION}")
     DataMapper::finalize
     DataMapper::auto_upgrade!
-  end
-
-  module Helpers
-    module Database
-      include Deltacloud::Database
-
-      def store_attributes_for(model, values={})
-        return if test_environment?
-        return if model.nil? or values.empty?
-        current_db.entities.first_or_create(:be_kind => model.to_entity, :be_id => model.id).update(values)
-      end
-
-      def load_attributes_for(model)
-        return {} if test_environment?
-        entity = get_entity(model)
-        entity.nil? ? {} : entity.to_hash
-      end
-
-      def delete_attributes_for(model)
-        return if test_environment?
-        entity = get_entity(model)
-        !entity.nil? && entity.destroy!
-      end
-
-      def get_entity(model)
-        current_db.entities.first(:be_kind => model.to_entity, :be_id => model.id)
-      end
-
-      def current_provider
-        Thread.current[:provider] || ENV['API_PROVIDER'] || 'default'
-      end
-
-      # This method allows to store things into database based on current driver
-      # and provider.
-      #
-      def current_db
-        Provider.first_or_create(:driver => driver_symbol.to_s, :url => current_provider)
-      end
-
-    end
   end
 
 end

commit e6c46db64f1a729dcd5a642b308f603b74f3600c

CIMI: fix typo introduced in 1a170507

server/lib/cimi/helpers/database_helper.rb
@@ -37,7 +37,7 @@ module Deltacloud
       # and provider.
       #
       def current_db
-        Provider.first_or_create(:driver => driver_symbol.to_s, :url => current_provider)
+        Deltacloud::Database::Provider.first_or_create(:driver => driver_symbol.to_s, :url => current_provider)
       end
 
     end