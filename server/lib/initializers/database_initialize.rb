92cf8dd4d6e87c28b4c502ff97ccd06fd24394d5
Michal Fojtik <mfojtik@redhat.com>
2013-02-19 10:07:21 +0100

    Core: The server will now exit when there are pending migrations
    + Added missing licenses to deltacloud-db-upgrade and mock_initialize

--- server/lib/initializers/database_initialize.rb
+++ server/lib/initializers/database_initialize.rb
@@ -1,3 +1,18 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.  The
+# ASF licenses this file to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
 # Initialize the storage layer we use to persist some CIMI entities
 # and attributes.
 #
@@ -38,8 +53,11 @@ DATABASE = Deltacloud::initialize_database
 DATABASE_MIGRATIONS_DIR = File.join(File.dirname(__FILE__), '..', '..', 'db', 'migrations')
 
 unless Sequel::Migrator.is_current?(DATABASE, DATABASE_MIGRATIONS_DIR)
-  warn "WARNING: The database needs to be upgraded. Run: 'deltacloud-db-upgrade' command."
-  DATABASE_UPGRADE = true
-else
-  DATABASE_UPGRADE = false
+  # Do not exit when this intitializer is included from deltacloud-db-upgrade
+  # script
+  #
+  unless ENV['DB_UPGRADE']
+    warn "WARNING: The database needs to be upgraded. Run: 'deltacloud-db-upgrade' command."
+    exit(1)
+  end
 end
