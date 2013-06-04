1a26f6ac73cd6db3c1a5842190f8b9734422a71e
David Ellinger <dellinger@ltionline.com>
2013-06-04 15:48:47 -0400

    Modified 'stop_instance' method to check if instance root type is ebs or instance_store. If ebs -- stop instance otherwise destroy instance

--- server/lib/deltacloud/drivers/ec2/ec2_driver.rb
+++ server/lib/deltacloud/drivers/ec2/ec2_driver.rb
@@ -333,11 +333,18 @@ module Deltacloud
         def stop_instance(credentials, instance_id)
           ec2 = new_client(credentials)
           #TODO: If image root is ebs (or persistent) then stop instance otherwise need to run "destroy_instance"
-          if ec2.stop_instances([instance_id])
-            instance(credentials, :id => instance_id)
+          #ec2_instance = ec2.instances[:instance_id]
+          #if ec2_instance.root_device_type == "ebs"
+          ec2_inst = ec2.describe_instances([instance_id]).first
+          if ec2_inst[:root_device_type] == "ebs"
+            if ec2.stop_instances([instance_id])
+              instance(credentials, :id => instance_id)
+            else
+              raise Deltacloud::BackendError.new(500, "Instance", "Instance stop failed", "")
+            end
           else
-            raise Deltacloud::BackendError.new(500, "Instance", "Instance stop failed", "")
-          end
+            destroy_instance(credentials,instance_id)
+          end  
         end
 
 
